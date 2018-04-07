ce84b22b834a3173fdc0f9029e2ffb2a684862af
Tassel <1141320106qq.com>
2018-04-07 13:58:33 +0800

    number

--- Project2/First.cpp
+++ Project2/First.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 int main()
 {
-	cout << "Hello World" << endl;
+	int i = 0;
+	cout << "请输入一个数字" << endl;
+	cin >> i;
+	cout << "你输入的数字是" << i;
+	/*cout << "Hello World" << endl;*/
 	system("pause");
 	return 0;
 }
